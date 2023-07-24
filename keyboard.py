4e10acb3d1ada0521de6f4d44360e7ed16f76add
crusher131 <jefe.zaca@icloud.com>
2023-07-24 16:57:17 -0300

    Agora não é mais necessário utilizar código hex para as funções, o código traduz automaticamente a tecla para hex.

--- keyboard.py
+++ keyboard.py
@@ -3,20 +3,18 @@ import time
 
 SendInput = ctypes.windll.user32.SendInput
 
-W = 0x11
-A = 0x1E
-S = 0x1F
-D = 0x20
-UP = 0xC8
-LEFT = 0xCB
-RIGHT = 0xCD
-DOWN = 0xD0
-ENTER = 0x1C
-ESC = 0x01
-TWO = 0x03
+
+keylist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+              "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+                "ESC", "UP", "LEFT", "RIGHT", "DOWN", "ENTER", "LSHIFT", "SHIFT", "RSHIFT", "LCONTROL", "CONTROL", "RCONTROL"]
+hexlist = [0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1f, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C,
+            0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
+            0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x57, 0x58,
+            0x01, 0xC8, 0xCB, 0xCD, 0xD0, 0x1C, 0x2A, 0X2a, 0x36, 0x1D, 0x1D, 0x9D]
 
 
-# C struct redefinitions
+
 PUL = ctypes.POINTER(ctypes.c_ulong)
 
 
@@ -52,18 +50,18 @@ class Input_I(ctypes.Union):
 class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
-
-
-# Actuals Functions
-def key_down(hexKeyCode):
+#Pressiona Tecla
+def key_down(keyp):
+    hexKeyCode = gen_hex_from_key(str.upper(keyp))
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-
-def release_key(hexKeyCode):
+#Solta tecla
+def release_key(keyp):
+    hexKeyCode = gen_hex_from_key(str.upper(keyp))
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0,
@@ -71,17 +69,15 @@ def release_key(hexKeyCode):
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-
-def press(hexKeyCode, delay=0.2):
+#Pressiona e solta tecla
+def press(hexKeyCode, delay=0.5):
     key_down(hexKeyCode)
     time.sleep(delay)
     release_key(hexKeyCode)
 
-# directx scan codes
-# http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
-
-if __name__ == '__main__':
-        key_down(ENTER)
-        time.sleep(1)
-        release_key(ENTER)
-        time.sleep(1)
+#Transforma string em hex
+def gen_hex_from_key(key):
+    for i, elemento in enumerate(keylist):
+        if elemento == key:
+            return hexlist[i]
+    return None
